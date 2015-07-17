04b265fbd18be519f9133bd39dba054a182c0481
steven <stevendiao1@gmail.com>
2015-07-17 14:53:13 -0400

    added a template doesn't actually work gives a 404 page can't find the views

---
+++ flapper-news/routes/index.js
@@ -0,0 +1,68 @@
+var mongoose = require('mongoose');
+var Comment = mongoose.model('Comment');											
+var Post = mongoose.model('Post');
+
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+module.exports = router;
+/**
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+**/
