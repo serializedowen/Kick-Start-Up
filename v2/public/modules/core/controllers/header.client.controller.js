c2b69e2d198c47d7a5d822f381e47204b0428c87
serializedowen <wjh199455@gmail.com>
2015-08-02 15:32:49 -0400

    Friendslist in navbar

--- v2/public/modules/core/controllers/header.client.controller.js
+++ v2/public/modules/core/controllers/header.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus',
-	function($scope, $state, Authentication, Menus) {
+angular.module('core').controller('HeaderController', ['$scope', '$state', '$http', 'Authentication', 'Menus',
+	function($scope, $state, $http, Authentication, Menus) {
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
@@ -18,6 +18,18 @@ angular.module('core').controller('HeaderController', ['$scope', '$state', 'Auth
     $scope.search = function(){
       if ($scope.keyword){
         $state.go('search', {keyword: $scope.keyword});
+      }
+    };
+
+
+    $scope.getFriends = function(){
+      if (Authentication.user){
+        $http.get('/users/' + Authentication.user._id + '/friends').success(function(data){
+          $scope.friends = data;
+        }).error(function(){
+            $scope.error.mesage = 'failed to load friend list.'
+          }
+        )
       }
     };
 	}
--- v2/public/modules/core/controllers/home.client.controller.js
+++ v2/public/modules/core/controllers/home.client.controller.js
@@ -6,6 +6,11 @@ angular.module('core').controller('HomeController', ['$scope', '$stateParams', '
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 
+
+    $scope.postFilter = function(post){
+      return post.user.displayName.indexOf($scope.keyword) > -1 || post.title.indexOf($scope.keyword) > -1
+    }
+
     $scope.search = function(){
       $scope.keyword = $stateParams.keyword;
       $http.put('/search/' + $stateParams.keyword).success(function(data){
