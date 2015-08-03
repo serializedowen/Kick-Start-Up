e015b11634b9f352d0ccbefcf9418ac91a156d3b
serializedowen <wjh199455@gmail.com>
2015-08-03 19:11:47 -0400

    moved project

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-
-web: ./v2/node_modules/.bin/forever -m 5 server.js
+web: ./node_modules/.bin/forever -m 5 server.js
---
+++ public/modules/users/config/users.client.config.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// Config HTTP Error Handling
+angular.module('users')
+.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}])
+.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl){
+        var fd = new FormData();
+        fd.append('file', file);
+        console.log("headers");
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+        .success(function(){
+        })
+        .error(function(){
+        });
+    }
+}])
+.config(['$httpProvider',
+	function($httpProvider) {
+		// Set the httpProvider "not authorized" interceptor
+		$httpProvider.interceptors.push(['$q', '$location', 'Authentication',
+			function($q, $location, Authentication) {
+				return {
+					responseError: function(rejection) {
+						switch (rejection.status) {
+							case 401:
+								// Deauthenticate the global user
+								Authentication.user = null;
+
+								// Redirect to signin page
+								$location.path('signin');
+								break;
+							case 403:
+								// Add unauthorized behaviour 
+								break;
+						}
+
+						return $q.reject(rejection);
+					}
+				};
+			}
+		]);
+	}])
+  .filter('millSecondsToDays', function() {
+      return function(millseconds) {
+        var seconds = Math.floor(millseconds / 1000);
+        var days = Math.floor(seconds / 86400);
+        var timeString = '';
+        if(days > 0) timeString += (days > 1) ? (days + " days ") : (days + " day ")
+        else timeString = '< 1 day.';
+        return timeString;
+      }
+    }
+);
